1010afa10766b78aadccf2047864d55808fcf7de
Sens3lesss <soloarmin59@gmail.com>
2022-09-06 10:13:39 +0200

    Guy

---
+++ app/src/main/java/com/example/wecker/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.wecker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements OnCreate {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    }
---
+++ app/src/main/java/com/example/wecker/OnCreate.java
@@ -0,0 +1,7 @@
+package com.example.wecker;
+
+public interface OnCreate {
+
+
+
+}
