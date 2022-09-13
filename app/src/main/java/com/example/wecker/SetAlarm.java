7d60e97f07660fdd162ba6a19ba03eecd87b171b
Thai <hoa.nguyen2002@yahoo.de>
2022-09-13 20:56:28 +0200

    Merge remote-tracking branch 'origin/Armin' into rares

--- app/src/main/java/com/example/wecker/SetAlarm.java
+++ app/src/main/java/com/example/wecker/SetAlarm.java
@@ -1,8 +1,16 @@
 package com.example.wecker;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TimePicker;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class SetAlarm extends AppCompatActivity {
 
@@ -10,6 +18,9 @@ public class SetAlarm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_alarm);
-    }
 
+        TimePicker timePicker = (TimePicker) this.findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true); // 24H Mode.
+    }
 }
+
